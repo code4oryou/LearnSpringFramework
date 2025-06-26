f011ef1a8319087b02a55595c992c90a32db38a2
Ayush Mandal <ayushmandal0122@gmail.com>
2025-06-26 23:19:54 +0530

    learned context, bean name ref, parameters, method calls in spring

--- learn-spring-framework/src/main/java/com/code4oryou/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/code4oryou/learn_spring_framework/App02HelloWorldSpring.java
@@ -16,8 +16,19 @@ public class App02HelloWorldSpring {
 			new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
 		//2: Configure the things that we want Spring to manage - @Configuration
+		System.out.println(context.getBean("name"));
 		
+		System.out.println(context.getBean("age"));
 
+		System.out.println(context.getBean("person"));
+		
+		System.out.println(context.getBean("person2MethodCall")); // we can call the bean methods directly i.e., the names and age by this method also by using the name and age methods
+		
+		System.out.println(context.getBean("person3Parameters"));
+		
+		System.out.println(context.getBean("address2"));
+		
+//		System.out.println(context.getBean(Address.class)); // this is an alternative way of retreiving the same result using type
 	}
 
 }
--- learn-spring-framework/src/main/java/com/code4oryou/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/code4oryou/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,8 +1,48 @@
 package com.code4oryou.learn_spring_framework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+record Person(String name, int age, Address address) {};
+
+record Address(String firstLine, String city) {};
 
 @Configuration
 public class HelloWorldConfiguration {
 
+	@Bean
+	public String name() {
+		return "Ayush";
+	}
+	
+	@Bean
+	public int age() {
+		return 24;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Pushkar", 25, new Address("Alipurduar", "West Bengal"));
+	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address()); //name, age method call
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) { 
+		//name, age, address3
+		return new Person(name, age, address3); //instead of calling the methods directly like in person2MethodCall, we are calling parameters and this is an alternative way
+	}
+	
+	@Bean(name = "address2") // using name attribute we can also change the name of the method
+	public Address address() {
+		return new Address("Kolkata", "West Bengal");
+	}
+	
+	@Bean(name = "address3") // using name attribute we can also change the name of the method
+	public Address address3() {
+		return new Address("Siliguri", "West Bengal");
+	}
 }
